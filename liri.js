34eb9b999e2cbc1ff5e7fd45efb3a13aa528c4ee
Gary Marroquin <gary.marroquin@gmail.com>
2017-11-01 04:35:11 -0500

    keys added

---
+++ liri.js
@@ -0,0 +1,8 @@
+var twitter = require(twitter); 
+
+var client = new Twitter(twitterKeys)({
+	consumer_key: '<esJ4mZewLjTysgdn54hzLEo7I>',
+	consumer_secret: '<ksP6hptqRPkseJmwhqEDGSo1QKTLTydHVJrm9B4zQv7QqH1ITv>',
+	access_token_key: '<924887570143760384-VV0GhpqWPkMCop5hV0g0LN5Qw5lh4Jz>',
+	access_token_secret: '<VRvvnhcG04Ypgwf5e1fvjfxhD805Cp1R6Un9SXPTGOVH2>',
+});
